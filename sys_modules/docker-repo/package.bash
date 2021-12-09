4994da8454f46bba6af103a38e12a3044c89ec54
romain <romain.vermeulen@gmail.com>
2021-12-09 15:39:49 +0800

    Bug fix in Docker-repo module

--- sys_modules/docker-repo/package.bash
+++ sys_modules/docker-repo/package.bash
@@ -2,8 +2,7 @@
 # Installation of the docker repository required to install docker
 
 checkInstall() { 
-	# Assume the repository was already setup if Docker is installed
-	isCommand docker
+	[ -f /etc/apt/sources.list.d/docker.list ]
 }
 
 runInstall() {
@@ -11,10 +10,12 @@ runInstall() {
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
 		| sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 	# Setup the stable repository
-	echo \
-		"deb [arch=$(dpkg --print-architecture) \
-		signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
-		https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+	printf "deb [arch=%s signed-by=%s] %s %s %s" \
+		"$(dpkg --print-architecture)" \
+		"/usr/share/keyrings/docker-archive-keyring.gpg" \
+		"https://download.docker.com/linux/ubuntu" \
+		"$(lsb_release -cs)" \
+		"stable" \
 		| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 }
 
