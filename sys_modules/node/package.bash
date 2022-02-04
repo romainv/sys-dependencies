63a58c61bbacc0b0e63b62a36c67beb59e86d405
romain <romain.vermeulen@gmail.com>
2022-02-04 15:37:35 +0800

    Only source nvm if needed when checking for node installation

--- sys_modules/node/package.bash
+++ sys_modules/node/package.bash
@@ -6,19 +6,23 @@ checkInstall() {
 	local version="$2"
 	local versionFile
 	# Check if node command exists, and that the specified version is installed
-	# shellcheck source=/dev/null
-	source "$HOME/.nvm/nvm.sh" &> /dev/null # Source nvm
 	isCommand node && {
 		if versionFile=$(cd "$PWD_BACKUP" && upsearch ".nvmrc"); then
 			# If a version is configured in a .nvmrc file 
 			version=$(cat "$versionFile")
 			# Check if the version is installed (exit code >0 if not)
+			# shellcheck source=/dev/null
+			source "$HOME/.nvm/nvm.sh" &> /dev/null # Source nvm
 			nvm ls "$version" &> /dev/null
 		else # If no version was configured in a .nvmrc file
 			if [[ -n "$version" && "$version" != '*' ]]; then
 				# If a specific version was provided
+				# shellcheck source=/dev/null
+				source "$HOME/.nvm/nvm.sh" &> /dev/null # Source nvm
 				nvm ls "$version" &> /dev/null
 			elif [[ "$branch" != "node" ]]; then
+				# shellcheck source=/dev/null
+				source "$HOME/.nvm/nvm.sh" &> /dev/null # Source nvm
 				# If a specific branch was provided
 				nvm ls"$branch" &> /dev/null
 			else # No specific version required
